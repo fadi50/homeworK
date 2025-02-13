0b51f988c8c54ff685ff9f0ac6c34d51c5a7c47a
fadi50 <adiboughanem45@gmail.com>
2025-02-13 16:29:02 +0200

    exercise 3

--- homeworK.py
+++ homeworK.py
@@ -1,19 +1,11 @@
-age = int(input("Enter your age: "))
+username = input("Enter username: ")
+password = input("Enter password: ")
 
-if age < 5:
-    price = 0
-elif age <= 12:
-    price = 5
-elif age <= 60:
-    price = 10
+if username == "admin" and password == "1234":
+    print("Access granted")
 else:
-    price = 7
+    print("Access denied")
 
-print(f"The ticket price is ${price}.")
 
-number = int(input("Enter a number: "))
 
-if number % 2 == 0:
-    print("The number is even.")
-else:
-    print("The number is odd.")
+
